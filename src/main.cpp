9f2868f6463a887dfc549110e5d23d310fef3b04
Gabri <gabriavoz@gmail.com>
2022-04-16 19:43:03 +0200

    Marcar primer bit

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,24 @@
 
 #define BUZZER 8
 
-int normal_bit_tone = 1500;  //2000 hz
+int normal_bit_tone = 1500;  // hz
+int first_bit_tone = 1300;
 int duration;
 int bpm = 120;
+int counter;
 
 void setup() {
   duration = 6000/bpm;
+  counter = 1;
 }
 
 void loop() {
-  tone(BUZZER, normal_bit_tone, duration);
+  if (counter%4 == 1){
+    tone(BUZZER, first_bit_tone, duration);
+  }
+  else{
+    tone(BUZZER, normal_bit_tone, duration);
+  }
+  counter++;
   delay(60000/bpm);
 }
