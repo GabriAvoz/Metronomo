45df0f829820f0e6775b092adad6bb577c634899
Gabri <gabriavoz@gmail.com>
2022-04-16 14:30:33 +0200

    Beat con tiempo fijo

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
 
+#define BUZZER 8
+
+int normal_bit_tone = 1500;  //2000 hz
+int duration;
+int bpm = 120;
+
 void setup() {
-  // put your setup code here, to run once:
+  duration = 6000/bpm;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  tone(BUZZER, normal_bit_tone, duration);
+  delay(60000/bpm);
 }
